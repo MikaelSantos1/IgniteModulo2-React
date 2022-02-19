cd5256174748f537fd97f08625906e958b7abb68
MikaelSantos1 <mikaelsantos120@outlook.com>
2022-02-19 18:43:25 -0300

    feature button RADIO

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,11 +1,16 @@
 import Modal from 'react-modal'
-import { Container } from './styles'
+import { Container,TransactionTypeContainer ,RadioBox} from './styles'
 import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { useState } from 'react'
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [type,setType]= useState('deposit')
+
     return (
         <Modal
             isOpen={isOpen}
@@ -30,6 +35,26 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     type="number"
                     placeholder='Valor'
                 />
+
+                <TransactionTypeContainer>
+                    <RadioBox 
+                    type='button'
+                    onClick={()=>{setType('deposit')}}
+                    isActive={type==='deposit'}
+                    >
+                        <img src={incomeImg} alt="entradas" />
+                        <span>Entrada</span>
+                    </RadioBox>
+
+                    <RadioBox 
+                    type='button'
+                    onClick={()=>{setType('withdraw')}}
+                    isActive={type==='withdraw'}
+                    >
+                        <img src={outcomeImg} alt="saida" />
+                        <span>Saida</span>
+                    </RadioBox>
+                </TransactionTypeContainer>
 
                 <input
                     type="text"
